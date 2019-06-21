12b4569bafd5a30fed06e3e504881eec5d784e3e
Marie Otaki <otakimarie@gmail.com>
2019-06-21 11:23:32 -0700

    update css

--- src/App.js
+++ src/App.js
@@ -1,26 +1,92 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Note from './Note/Note';
+import NoteForm from './NoteForm/NoteForm';
+import { DB_CONFIG } from './Config/config';
+import firebase from 'firebase/app';
+import 'firebase/database';
 import './App.css';
+import logo from './Static/img/notebook.svg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.addNote = this.addNote.bind(this);
+    this.removeNote = this.removeNote.bind(this);
+
+    this.app = firebase.initializeApp(DB_CONFIG);
+    this.database = this.app.database().ref().child('notes');
+
+    // We're going to setup the React state of our component
+    this.state = {
+      notes: [],
+    }
+  }
+
+  componentWillMount(){
+    const previousNotes = this.state.notes;
+
+    // DataSnapshot
+    this.database.on('child_added', snap => {
+      previousNotes.push({
+        id: snap.key,
+        noteContent: snap.val().noteContent,
+      })
+
+      this.setState({
+        notes: previousNotes
+      })
+    })
+
+    this.database.on('child_removed', snap => {
+      for(var i=0; i < previousNotes.length; i++){
+        if(previousNotes[i].id === snap.key){
+          previousNotes.splice(i, 1);
+        }
+      }
+
+      this.setState({
+        notes: previousNotes
+      })
+    })
+  }
+
+  addNote(note){
+    this.database.push().set({ noteContent: note});
+  }
+
+  removeNote(noteId){
+    console.log("from the parent: " + noteId);
+    this.database.child(noteId).remove();
+  }
+
+  render() {
+    return (
+      <div className="notesWrapper">
+        <div className="notesHeader">
+          <div className="heading">
+            <img src={logo} alt="Logo" />
+            React & Firebase To-Do List
+          </div>
+        </div>
+        <div className="notesBody">
+          {
+            this.state.notes.map((note) => {
+              return (
+                <Note noteContent={note.noteContent} 
+                noteId={note.id} 
+                key={note.id} 
+                removeNote ={this.removeNote}/>
+              )
+            })
+          }
+        </div>
+        <div className="notesFooter">
+          <NoteForm addNote={this.addNote} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Config/config.js
@@ -0,0 +1,9 @@
+export const DB_CONFIG = {
+  apiKey: "AIzaSyBWBrKbyhuHELLrlo-xQbmnjQTgtQLDNIU",
+  authDomain: "reactnote-93fb1.firebaseapp.com",
+  databaseURL: "https://reactnote-93fb1.firebaseio.com",
+  projectId: "reactnote-93fb1",
+  storageBucket: "reactnote-93fb1.appspot.com",
+  messagingSenderId: "578642209243",
+  appId: "1:578642209243:web:236718b46208dc33"
+};
